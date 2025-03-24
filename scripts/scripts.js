ea8e0ca3e9422bcfccca1e14b0696b04149e4de3
FLX <cauerast@gmail.com>
2025-03-24 20:35:31 -0300

    js

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+
+
+function calcular(){
+    let acaoSocial = document.getElementById("acaoSocial").value;
+    let homenagem = document.getElementById("homenagem").value;
+    let soma = acaoSocial + homenagem;
+alert(soma)
+}
+
