436b525d61846e1268fce0baaa211ba66cd69494
FLX <cauerast@gmail.com>
2025-05-19 18:06:13 -0300

    a

--- EstruturaRepeticao/js/ex5.js
+++ EstruturaRepeticao/js/ex5.js
@@ -1,12 +1,1 @@
 // ex 5 by cauerast
-
-function tabuadaX(){
-    let number = Number(prompt(`Digite um numero e veja seus multiplos: `));
-    let table = `Tabuada do numero ${number}!\n\n`;
-    for(let count = 0; count <= 10; count++){
-        table += `${number} x ${count} = ${number * count}\n`;
-
-    }
-    window.alert(table);
-
-}
