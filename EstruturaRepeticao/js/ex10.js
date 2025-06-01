14038310db9a66d68b86f8fb9438430c72a12e3c
FLX <cauerast@gmail.com>
2025-06-01 02:59:16 -0300

    ex10

--- EstruturaRepeticao/js/ex10.js
+++ EstruturaRepeticao/js/ex10.js
@@ -1,9 +1,38 @@
 // ex 10 by cauerast
 
-function numberX(){
-    let i = 0
-
+function SomaParEPrimos(){
+    let i = 0;
+    let k = 0;
+    let numero = 0;
+    let somaPar = 0;
+    let somaPrimos = 0;
+    
     for(i = 1; i <= 10; i++){
-        numero
+        do {
+            numero = Number(prompt(`[${i}] - Insira um número -> `));
+        } while (numero < 0 || isNaN(numero)); 
+        
+        
+        if(numero % 2 == 0){
+            somaPar += numero;
+        }
+        
+
+        let divisores = 0;
+
+        for(k = 1; k <= numero; k++){
+            if(numero % k == 0){
+                divisores++;
+            }
+        }
+
+        if(divisores == 2){
+            somaPrimos += numero;
+        }
+
+
     }
+
+    window.alert(`Soma dos numeros pares: ${somaPar}`);
+    window.alert(`Soma dos numeros primos: ${somaPrimos}`);
 }
