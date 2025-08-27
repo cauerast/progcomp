3f50ac4e20817e9f401a3e5fcd2f63241fd9b4ac
FLX <cauerast@gmail.com>
2025-08-27 20:35:45 -0300

    update

---
+++ String/js/ex1.js
@@ -0,0 +1,13 @@
+// ex 1 by Cauê Silva Rasteiro - 26033 - C.C
+
+let frase = "estou estudando js";
+
+console.log(frase.charAt(0));
+console.log(frase[4]);
+console.log(frase.length);
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase().includes("JS"));
+console.log(frase.toLowerCase().includes("js"));
+console.log(frase.slice(0, 5)); // pega o char 0, 1, 2, 3 e 4;
+console.log(frase.slice(-5)); // pega os ultimos 5 char
