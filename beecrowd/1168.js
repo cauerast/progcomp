173e4f9965e64f627ec97411fb3f02657af351ba
FLX <cauerast@gmail.com>
2025-09-04 17:58:29 -0300

    update

--- beecrowd/1168.js
+++ beecrowd/1168.js
@@ -1,3 +1,24 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
+var lines = input.trim().split('\n');
+
+let cases = parseInt(lines[0]);
+let valor = [];
+let valorUni = [];
+
+//
+
+for(let i = 1; i <= cases; i++){
+    valor[i] = parseInt(lines[i]);
+    valorUni[i] = valor[i].split(""); // array de arrays = matriz
+
+}
+
+for(let i = 1; i <= cases; i++){ 
+    for(x){
+
+    }
+}
+
+
+
 
