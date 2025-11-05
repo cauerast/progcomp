2b1a88ab3961a4db9e721d653c7bbc77ecc990f8
FLX <cauerast@gmail.com>
2025-11-05 20:21:31 -0300

    update

---
+++ ES5 and ES6/ex analise de vendas.js
@@ -0,0 +1,8 @@
+let vendas = [
+    { produto: "Notebook", categoria: "Informática", valor: 3500, quantidade: 2 },
+    { produto: "Mouse", categoria: "Informática", valor: 80, quantidade: 5 },
+    { produto: "Camiseta", categoria: "Moda", valor: 50, quantidade: 10 },
+    { produto: "Tênis", categoria: "Moda", valor: 250, quantidade: 4 },
+    { produto: "Livro", categoria: "Educação", valor: 40, quantidade: 7 },
+    { produto: "Tablet", categoria: "Informática", valor: 1500, quantidade: 1 }
+];
