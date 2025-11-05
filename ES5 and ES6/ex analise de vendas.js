74ecafb4a7217f28ba33314d1fa8c4898c847dab
cauerast <cauerast@gmail.com>
2025-11-05 20:47:29 -0300

    update

--- ES5 and ES6/ex analise de vendas.js
+++ ES5 and ES6/ex analise de vendas.js
@@ -6,3 +6,46 @@ let vendas = [
     { produto: "Livro", categoria: "Educação", valor: 40, quantidade: 7 },
     { produto: "Tablet", categoria: "Informática", valor: 1500, quantidade: 1 }
 ];
+
+let vendasInformatica = 0
+let vendasModa = 0
+let vendasEducacao = 0
+let maiorCatVendas = "";
+let maiorValorVendas = 0
+
+vendas.forEach((venda) => {
+    if(venda.categoria == "Informática"){
+        vendasInformatica += (venda.valor * venda.quantidade)
+    }
+
+    if(venda.categoria == "Moda"){
+        vendasModa += (venda.valor * venda.quantidade)
+    }
+
+    if(venda.categoria == "Educação"){
+        vendasEducacao += (venda.valor * venda.quantidade)
+    }
+})
+
+if(vendasInformatica > vendasEducacao && vendasInformatica > vendasModa){
+    maiorCatVendas += "Informatica"
+    maiorValorVendas += vendasInformatica
+
+}
+if(vendasModa > vendasEducacao && vendasModa > vendasInformatica){
+    maiorCatVendas += "Moda"
+    maiorValorVendas += vendasModa
+
+}
+if(vendasEducacao > vendasInformatica && vendasEducacao > vendasModa){
+    maiorCatVendas += "Educacao"
+    maiorValorVendas += vendasEducacao
+
+}
+
+console.log(`total de vendas por categoria: -> \n 
+    vendas Informatica: R$${vendasInformatica.toFixed(2)}\n
+    vendas Moda: R$${vendasModa.toFixed(2)}\n
+    vendas Educacao: R$${vendasEducacao.toFixed(2)}\n`)
+
+console.log(`categoria com maior valor de vendas: ${maiorCatVendas} (R$${maiorValorVendas})`)
